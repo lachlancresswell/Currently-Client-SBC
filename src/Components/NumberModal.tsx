015cb51c0457918237167d7b236909556d8ef01b
Lachlan <lachy93@gmail.com>
2023-11-14 21:18:28 +0000

    refactor (NumberModal)

--- src/Components/NumberModal.tsx
+++ src/Components/NumberModal.tsx
@@ -4,41 +4,67 @@ import '../Styles/ConfigModal.css'
 import { ConfigVariable } from '../../Types';
 import { ArrowUp, ArrowDown } from "./CommonUI";
 
-export const NumberModal = ({ setting, onClose, onSubmit, updated }: { setting: ConfigVariable, onClose: () => void, onSubmit: (setting: ConfigVariable) => void, updated?: boolean }) => {
+/**
+ * Validate and update a number value.
+ * @param setting The setting to validate against
+ * @param newValue The new value to validate
+ * @returns The updated value
+ */
+export const validateAndUpdateValue = (setting: ConfigVariable, newValue: number) => {
+    if (setting.min !== undefined && newValue < setting.min) {
+        return setting.min;
+    }
+
+    if (setting.max !== undefined && newValue > setting.max) {
+        return setting.max;
+    }
+
+    return newValue;
+}
+
+/**
+ * A component for displaying and manipulating a number.
+ * @param setting The setting to display
+ * @param onClose The function to call when the modal is closed
+ * @param onSubmit The function to call when the modal is submitted
+ * @param updated Optional: whether the setting has been updated
+ * @returns A React component
+ */
+export const NumberModal = ({
+    setting,
+    onClose,
+    onSubmit,
+    updated
+}: {
+    setting: ConfigVariable,
+    onClose: () => void,
+    onSubmit: (setting: ConfigVariable) => void,
+    updated?: boolean
+}) => {
     const [value, setValue] = useState<number>(parseInt(setting.value || '0'));
     const [startValue] = useState<number>(value);
 
-    const resetValue = () => {
-        setValue(startValue);
-    }
+    const resetValue = () => setValue(startValue);
 
-    const isDisabled = () => {
-        return updated || (value === startValue)
-    }
-
-    const validateAndUpdateValue = (newValue: number) => {
-        if (setting.min !== undefined && newValue < setting.min) {
-            return setValue(setting.min);
-        }
-
-        if (setting.max !== undefined && newValue > setting.max) {
-            return setValue(setting.max);
-        }
-
-        return setValue(newValue);
-    }
+    const isDisabled = () => updated || (value === startValue)
 
     return (
         <div className='gridModal-flex'>
             <Value onChange={(e) => setValue(parseInt(e.target.value))} value={value} max={setting.max} />
             <div className='modal-flex-1'>
-                <ArrowUp onClick={() => validateAndUpdateValue(value + 1)} />
-                <ArrowDown onClick={() => validateAndUpdateValue(value - 1)} />
+                <ArrowUp onClick={() => setValue(validateAndUpdateValue(setting, value + 1))} />
+                <ArrowDown onClick={() => setValue(validateAndUpdateValue(setting, value - 1))} />
             </div>
             <div className='modal-flex-1'>
-                <button className='modal-button-method-flex modal-button-x' onClick={() => onClose()}>X</button>
-                <button className='modal-button-method-flex input-modified-reset' disabled={isDisabled()} onClick={resetValue}><RefreshIcon /></button>
-                <button className="modal-button-method-flex input-modified" disabled={isDisabled()} onClick={() => onSubmit({ ...setting, ...{ value: value.toString() } })}>OK</button>
+                <button className='modal-button-method-flex modal-button-x' onClick={() => onClose()} data-testid={'Close-button'}>
+                    X
+                </button>
+                <button className='modal-button-method-flex input-modified-reset' disabled={isDisabled()} onClick={resetValue} data-testid={'Refresh-button'}>
+                    <RefreshIcon />
+                </button>
+                <button className="modal-button-method-flex input-modified" disabled={isDisabled()} onClick={() => onSubmit({ ...setting, ...{ value: value.toString() } })} data-testid={'OK-button'}>
+                    OK
+                </button>
             </div>
         </div >
     )
@@ -53,7 +79,9 @@ const Value = ({ value, max, onChange }: { value: number | string, max?: number,
                 type='number'
                 value={value}
                 onChange={onChange}
-                min="0" max={max.toString()}
+                min="0"
+                max={max.toString()}
+                data-testid={'Value-input'}
             />
         </div>
     )
