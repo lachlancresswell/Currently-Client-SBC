67529b0da260f45b51bc360475558a848ed9113c
Lachlan <lachy93@gmail.com>
2023-11-14 20:09:52 +0000

    tests (CommonUI): add test ids

--- src/Components/CommonUI.tsx
+++ src/Components/CommonUI.tsx
@@ -1,15 +1,19 @@
 export const ArrowRight = ({ onClick }: { onClick?: React.MouseEventHandler; }) => {
-    return <Arrow onClick={onClick} text='▶' />;
+    return <Arrow onClick={onClick} text='▶' dataTestId="ArrowRight" />;
 };
 export const ArrowLeft = ({ onClick }: { onClick?: React.MouseEventHandler; }) => {
-    return <Arrow onClick={onClick} text='◀' />;
+    return <Arrow onClick={onClick} text='◀' dataTestId="ArrowLeft" />;
 };
 export const ArrowUp = ({ onClick }: { onClick?: React.MouseEventHandler; }) => {
-    return <Arrow onClick={onClick} text='⬆' />;
+    return <Arrow onClick={onClick} text='⬆' dataTestId="ArrowUp" />;
 };
 export const ArrowDown = ({ onClick }: { onClick?: React.MouseEventHandler; }) => {
-    return <Arrow onClick={onClick} text='⬇' />;
+    return <Arrow onClick={onClick} text='⬇' dataTestId="ArrowDown" />;
 };
-export const Arrow = ({ onClick, text }: { onClick?: React.MouseEventHandler; text: string; }) => {
-    return <button className='span-one-modal modal-button-arrow' onClick={onClick}>{text}</button>;
+export const Arrow = ({ onClick, text, dataTestId }: {
+    onClick?: React.MouseEventHandler,
+    text: string,
+    dataTestId?: string,
+}) => {
+    return <button className='span-one-modal modal-button-arrow' onClick={onClick} data-testid={dataTestId}>{text}</button>;
 };
